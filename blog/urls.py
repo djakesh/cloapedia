8532d713cde03690111d428c8ab56935dc5eb7d3
djakesh <dzhakeshev.turgut@gmail.com>
2021-08-09 18:28:32 +0600

    added update delete posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import main_page, create_post, my_posts, category_posts, search
+from .views import (
+    main_page, create_post, my_posts, category_posts,
+    search, post_detail, post_update, post_delete)
 
 urlpatterns = [
+    path('post_delete/<int:pk>/', post_delete, name='post_delete'),
+    path('post_update/<int:pk>/',post_update, name='post_update'),
+    path('post_detail/<int:pk>/', post_detail, name='post_detail'),
     path('category/<int:pk>/', category_posts, name='category_details'),
     path('my-posts/', my_posts, name='my_posts'),
     path('create-post/', create_post, name='post_create'),
